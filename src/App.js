7f1166cf6d9dcd2c9c2c8683d994a64f6da47d0a
Biswarup Chatterjee <chatterjeebiswarup61@gmail.com>
2024-03-29 17:46:23 +0530

    Third assigment created

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 // import Assignment1 from "./R1S12/Assignment1"
-import Assignment2 from "./R1S12/Assignment2"
-// import Assignment2 from "./R1S12/Assignment3"
-// import Assignment2 from "./R1S12/Assignment4"
-// import Assignment2 from "./R1S12/Assignment5"
-// import Assignment2 from "./R1S12/Assignment6"
-// import Assignment2 from "./R1S12/Assignment7"
-// import Assignment2 from "./R1S12/Assignment8"
-// import Assignment2 from "./R1S12/Assignment9"
-// import Assignment2 from "./R1S12/Assignment10"
-// import Assignment2 from "./R1S12/Assignment11"
-// import Assignment2 from "./R1S12/Assignment12"
-// import Assignment2 from "./R1S12/Assignment13"
-// import Assignment2 from "./R1S12/Assignment14"
-// import Assignment2 from "./R1S12/Assignment15"
+// import Assignment2 from "./R1S12/Assignment2"
+import Assignment3 from "./R1S12/Assignment3"
+// import Assignment4 from "./R1S12/Assignment4"
+// import Assignment5 from "./R1S12/Assignment5"
+// import Assignment6 from "./R1S12/Assignment6"
+// import Assignment7 from "./R1S12/Assignment7"
+// import Assignment8 from "./R1S12/Assignment8"
+// import Assignment9 from "./R1S12/Assignment9"
+// import Assignment10 from "./R1S12/Assignment10"
+// import Assignment11 from "./R1S12/Assignment11"
+// import Assignment12 from "./R1S12/Assignment12"
+// import Assignment13 from "./R1S12/Assignment13"
+// import Assignment14 from "./R1S12/Assignment14"
+// import Assignment15 from "./R1S12/Assignment15"
 
 export default function App() {
   return (
@@ -20,20 +20,20 @@ export default function App() {
       <h1>
         Assignments
         {/* <Assignment1 /> */}
-        <Assignment2 />
-        {/* <Assignment3 />
-        <Assignment4 />
-        <Assignment5 />
-        <Assignment6 />
-        <Assignment7 />
-        <Assignment8 />
-        <Assignment9 />
-        <Assignment10 />
-        <Assignment11 />
-        <Assignment12 />
-        <Assignment13 />
-        <Assignment14 />
-        <Assignment15 /> */}
+        {/* <Assignment2 /> */}
+        <Assignment3 />
+        {/* <Assignment4 /> */}
+        {/* <Assignment5 /> */}
+        {/* <Assignment6 /> */}
+        {/* <Assignment7 /> */}
+        {/* <Assignment8 /> */}
+        {/* <Assignment9 /> */}
+        {/* <Assignment10 /> */}
+        {/* <Assignment11 /> */}
+        {/* <Assignment12 /> */}
+        {/* <Assignment13 /> */}
+        {/* <Assignment14 /> */}
+        {/* <Assignment15 />  */}
       </h1>
     </div>
   )
---
+++ src/R1S12/Assignment3.js
@@ -0,0 +1,64 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+export default function Assignment3() {
+    const [id, setId] = useState('')
+    const [user, setUser] = useState([])
+    const [userDetails, setUserDetails] = useState(null)
+    useEffect(() => {
+        axios.get(`https://jsonplaceholder.typicode.com/users`)
+            .then((response) => {
+                const result = response.data
+                setUser(result)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }, [])
+
+    useEffect(() => {
+        if (id) {
+            axios.get(`https://jsonplaceholder.typicode.com/users/${id}`)
+                .then((response) => {
+                    const result = response.data
+                    setUserDetails(result)
+                    console.log(userDetails)
+                })
+                .catch((err) => {
+                    console.log(err)
+                })
+        } else {
+            setUserDetails(null)
+        }
+    }, [id])
+
+    const handleChange = (e) => {
+        setId(e.target.value)
+    }
+
+    return (
+        <div>
+            <h1>
+                Assignment3
+            </h1>
+            <label htmlFor="user"></label>
+            <select name="" id="user" onChange={handleChange}>
+                <option value="">Select User</option>
+                {user.map((ele) => {
+                    return (
+                        <option value={ele.id} key={ele.id}>{ele.name}</option>
+                    )
+                })}
+            </select>
+            {userDetails ? (
+                <div>
+                    <h2>User Name: {userDetails.name}</h2>
+                    <h2>User Email: {userDetails.email}</h2>
+                    <h2>User City: {userDetails.address.city}</h2>
+                </div>
+            ) : (
+                <p>Select a User</p>
+            )}
+        </div>
+    )
+}
