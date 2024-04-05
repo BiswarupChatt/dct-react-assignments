0dbdef7053598c9fa13d6e45b5d05dcb978ac42d
Biswarup Chatterjee <chatterjeebiswarup61@gmail.com>
2024-04-05 22:01:36 +0530

    twelveth edit

--- src/R1S12/Assignment12.js
+++ src/R1S12/Assignment12.js
@@ -11,18 +11,20 @@ export default function Assignment12 (){
     }
     return(
         <div>
-            <h1>
-                Assignment 12
-            </h1>
-            <select name="" onChange={handleChange}>
-                <option value="">Select Color</option>
-               { colorList.map((ele, i)=>{
-                    return(
-                        <option key={i} value={ele}>{ele}</option>
-                    )
-                })}
-            </select>
-            <div style={{backgroundColor: color, width: '100%', height: '100vh',}}></div>
+            <div style={{position: 'relative', zIndex:1}}>
+                <h1>
+                    Assignment 12
+                </h1>
+                <select name="" onChange={handleChange} >
+                    <option value="">Select Color</option>
+                   { colorList.map((ele, i)=>{
+                        return(
+                            <option key={i} value={ele}>{ele}</option>
+                        )
+                    })}
+                </select>
+            </div>
+            <div style={{backgroundColor: color, width: '100%', height: '100vh', position: 'absolute', left: 0, top: 0}}></div>
         </div>
     )
 }
